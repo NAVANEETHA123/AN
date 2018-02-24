efdf62910fe40a427372d696fbf69d16e54a4cf0
NAVANEETHA123 <35696858+NAVANEETHA123@users.noreply.github.com>
2018-02-24 09:18:37 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,4 @@
+a=int(input("enter a value :")
+b=int(input("enter b value :")
+c=int(a)+int(b)
+print(c)
---
+++ frng.py
@@ -0,0 +1,6 @@
+for i in [1,2,3,4,5,6,7,8,9,]:
+    if i==1:
+        break
+    print(i)
+print("out of for loop")
+print(i)
