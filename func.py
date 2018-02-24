254cfeaf5320d0b82cea442de8179ccd36bbd4ab
NAVANEETHA123 <35696858+NAVANEETHA123@users.noreply.github.com>
2018-02-24 09:07:15 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+print(add(4,6))
+print(sub(4,6))
+print(mul(4,6))
+print(div(4,6))
---
+++ game.py
@@ -0,0 +1,24 @@
+import random
+r=("rock","paper","scissor")
+computer=random.choice(r)
+ print("rock","scissor","paper")
+ user=input("press q to enter your choice")
+    if computer==user:
+        print("rock")
+        print("it is a tie")
+    elif computer=="scissor":
+        print("user won")
+    else:
+        print("computer won")
+    if computer==user:
+        print("paper")
+        print("it is a tie")
+    elif computer=="rock"
+        print("user won")
+    else:
+        print("computer won")
+    if computer==user:
+        print("scissor")
+    
+
+   
