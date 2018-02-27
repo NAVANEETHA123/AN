ee436be23e83cd5d3c74a9b9ab7de269e5ea321e
NAVANEETHA123 <35696858+NAVANEETHA123@users.noreply.github.com>
2018-02-27 10:32:56 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,6 +1,18 @@
-for i in [1,2,3,4.5]:
-    if i==5:
-        break
-    print(i)
-print("out of for loop")
-print(i)
+# importing the module
+import tweepy
+ 
+# personal details
+consumer_key ="zQRMoTIlyVUV0Pxeex9YoCzYS"
+consumer_secret ="PYwcSMzvgeT5yLAZkQtnMnj0BbotCR7ee0G2Ykz3CrtkUKgYxv"
+access_token ="968078089912766464-Zvw0xtO8C5hNFSRkdQYBcwY3hMWUfFa"
+access_token_secret ="aXExcoNfbWtf8kVitCqCnw3aKMDDGpAhPGDH0M2oUBFqK"
+  
+# authentication of consumer key and secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+ 
+# authentication of access token and secret
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+ 
+# update the status
+api.update_status(status ="Hello Everyone !")
--- 2.py
+++ 2.py
@@ -1,6 +1,18 @@
-pw="7788"
-a=input("enter pglab password")
-if a==pw:
-    print("password is correct")
-else:
-   print("password is wrong")
+# importing the module
+import tweepy
+ 
+# personal details
+consumer_key ="zQRMoTIlyVUV0Pxeex9YoCzYS"
+consumer_secret ="PYwcSMzvgeT5yLAZkQtnMnj0BbotCR7ee0G2Ykz3CrtkUKgYxv"
+access_token ="968078089912766464-Zvw0xtO8C5hNFSRkdQYBcwY3hMWUfFa"
+access_token_secret ="aXExcoNfbWtf8kVitCqCnw3aKMDDGpAhPGDH0M2oUBFqK"
+ 
+# authentication of consumer key and secret
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+ 
+# authentication of access token and secret
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+ 
+# update the status
+api.update_status(status="Hello !")
