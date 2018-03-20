6cd9d6cf83c263e5fb133241b7456330a8997d2f
NAVANEETHA123 <35696858+NAVANEETHA123@users.noreply.github.com>
2018-03-20 10:16:25 +0530

    Add files via upload

---
+++ 111.py
@@ -0,0 +1,115 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: /home/cs2017a119/nava1.py =====================
+pic taken
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1303, in _pack_image
+    if os.path.getsize(filename) > (max_size * 1024):
+  File "/usr/lib/python3.6/genericpath.py", line 50, in getsize
+    return os.stat(filename).st_size
+FileNotFoundError: [Errno 2] No such file or directory: '/home/cs2017019/Desktop/img1.jpg'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/cs2017a119/nava1.py", line 23, in <module>
+    api. update_with_media(img, status="nice one")
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 219, in update_with_media
+    headers, post_data = API._pack_image(filename, 3072, form_field='media[]', f=f)
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1306, in _pack_image
+    raise TweepError('Unable to access file: %s' % e.strerror)
+tweepy.error.TweepError: Unable to access file: No such file or directory
+>>> 
+===================== RESTART: /home/cs2017a119/nava1.py =====================
+pic taken
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1303, in _pack_image
+    if os.path.getsize(filename) > (max_size * 1024):
+  File "/usr/lib/python3.6/genericpath.py", line 50, in getsize
+    return os.stat(filename).st_size
+FileNotFoundError: [Errno 2] No such file or directory: '/home/cs2017019/Desktop/img1.jpg'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/cs2017a119/nava1.py", line 22, in <module>
+    api. update_with_media(img, status="nice one")
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 219, in update_with_media
+    headers, post_data = API._pack_image(filename, 3072, form_field='media[]', f=f)
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1306, in _pack_image
+    raise TweepError('Unable to access file: %s' % e.strerror)
+tweepy.error.TweepError: Unable to access file: No such file or directory
+>>> 
+===================== RESTART: /home/cs2017a119/nava1.py =====================
+pic taken
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1303, in _pack_image
+    if os.path.getsize(filename) > (max_size * 1024):
+  File "/usr/lib/python3.6/genericpath.py", line 50, in getsize
+    return os.stat(filename).st_size
+FileNotFoundError: [Errno 2] No such file or directory: '/home/cs2017a119/Desktop/img1.jpg'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/cs2017a119/nava1.py", line 22, in <module>
+    api. update_with_media(img, status="nice one")
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 219, in update_with_media
+    headers, post_data = API._pack_image(filename, 3072, form_field='media[]', f=f)
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1306, in _pack_image
+    raise TweepError('Unable to access file: %s' % e.strerror)
+tweepy.error.TweepError: Unable to access file: No such file or directory
+>>> 
+===================== RESTART: /home/cs2017a119/nava1.py =====================
+pic taken
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1303, in _pack_image
+    if os.path.getsize(filename) > (max_size * 1024):
+  File "/usr/lib/python3.6/genericpath.py", line 50, in getsize
+    return os.stat(filename).st_size
+FileNotFoundError: [Errno 2] No such file or directory: '/home/cs2017a119/Desktop/img1.jpg'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/cs2017a119/nava1.py", line 22, in <module>
+    api. update_with_media(img, status="nice one")
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 219, in update_with_media
+    headers, post_data = API._pack_image(filename, 3072, form_field='media[]', f=f)
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1306, in _pack_image
+    raise TweepError('Unable to access file: %s' % e.strerror)
+tweepy.error.TweepError: Unable to access file: No such file or directory
+>>> 
+===================== RESTART: /home/cs2017a119/nava1.py =====================
+pic taken
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1303, in _pack_image
+    if os.path.getsize(filename) > (max_size * 1024):
+  File "/usr/lib/python3.6/genericpath.py", line 50, in getsize
+    return os.stat(filename).st_size
+FileNotFoundError: [Errno 2] No such file or directory: '/home/cs2017a119/Desktop/img1.jpg'
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "/home/cs2017a119/nava1.py", line 22, in <module>
+    api. update_with_media(img, status="nice one")
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 219, in update_with_media
+    headers, post_data = API._pack_image(filename, 3072, form_field='media[]', f=f)
+  File "/usr/local/lib/python3.6/dist-packages/tweepy/api.py", line 1306, in _pack_image
+    raise TweepError('Unable to access file: %s' % e.strerror)
+tweepy.error.TweepError: Unable to access file: No such file or directory
+>>> 
+===================== RESTART: /home/cs2017a119/nava1.py =====================
+pic taken
+wait for 5 seconds for selfie
+success
+pic taken
+wait for 5 seconds for selfie
+success
+pic taken
+wait for 5 seconds for selfie
+success
+>>> 
